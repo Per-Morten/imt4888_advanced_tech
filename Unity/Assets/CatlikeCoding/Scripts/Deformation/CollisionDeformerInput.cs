5b388b1f547ff3d5f52f56eb97e7ad152be39e18
Per-Morten Straume <p.m.straume@outlook.com>
2018-10-25 22:53:25 +0200

    Add simple deformer on collision

---
+++ Unity/Assets/CatlikeCoding/Scripts/Deformation/CollisionDeformerInput.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class CollisionDeformerInput : MonoBehaviour
+{
+    bool mIsColliding = false;
+    ContactPoint[] mContactPoints;
+
+    IDeformer mDeformer;
+
+    private void Start()
+    {
+         mDeformer = GetComponent<IDeformer>();
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Enter");
+        mIsColliding = true;
+        mContactPoints = collision.contacts;
+        //Debug.Log("Enter");
+        //collision.rigidbody.isKinematic = false;
+        //rb.isKinematic = false;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Exit");
+
+        mIsColliding = false;
+        mContactPoints = collision.contacts;
+        //Debug.Log("Leave");
+        //rb.isKinematic = true;
+        //collision.rigidbody.isKinematic = true;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Stay");
+        mContactPoints = collision.contacts;
+        // Debug-draw all contact points and normals
+        //foreach (ContactPoint contact in collisions.contacts)
+        //{
+        //    Debug.Log("Collision");
+        //    DebugLines.DrawLine(contact.point, contact.point + contact.normal, Color.red);
+        //    //Debug.DrawRay(contact.point, contact.normal * 10, Color.white);
+        //}
+    }
+
+    private void Update()
+    {
+        if (mIsColliding)
+        {
+            foreach (var contact in mContactPoints)
+            {
+                var point = contact.point;
+                point -= contact.normal * 0.1f;
+                mDeformer.AddDeformingForce(point, 10.0f);
+                DebugLines.DrawLine(transform.position, point, Color.red);
+            }
+        }
+    }
+
+
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    RaycastHit hit;
+    //    if (Physics.Raycast(transform.position, transform.position - other.transform.position, out hit))
+    //    {
+    //        Debug.Log("Point of contact: " + hit.point);
+    //        DebugLines.DrawLine(transform.position, hit.collider.gameObject.transform.position, Color.blue);
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Not hit");
+    //    }
+
+    //    //        other.attachedRigidbody.
+
+    //    Debug.Log("Triggered");
+    //}
+}
